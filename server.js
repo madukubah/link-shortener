fa13dbae04645a0fc32fa26647142c17b12749fe
madukubah <muhalfalah1998@gmail.com>
2022-01-12 11:55:02 +0800

    update

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ env.config()
 const mongo = require('./db/mongo.js')
 const app = require('./app')
 const port = process.env.PORT
+const appIp = process.env.APP_IP
 const server = http.createServer(app)
 
 mongo.connect().then(() => {
-    server.listen(port, () => {
+    server.listen(port, appIp, () => {
         console.log('listening on port: '+port)
     })
 })
